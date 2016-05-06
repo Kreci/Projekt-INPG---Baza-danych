1eae8b9d0f1bf6dd603b5de754d0492d8870d54c
Kreci <olson3344@gmail.com>
2016-05-06 19:50:03 +0200

    Added files via upload
    Funkcje znajdujące i wypisujące pierwszą, ostatnią i wszystkie nazwy gatunków (alfabetycznie)

---
+++ minmaxinorder.c
@@ -0,0 +1,38 @@
+void findMin(node*)
+    {
+        if(node == NULL)
+            {
+                printf("Drzewo jest puste!\n");
+                return;
+            }
+        if(node->left != NULL)
+            return findMin(node->left);
+        else
+            printf("Pierwszy rekord to %s\n", node->entry.stdName);
+        return;
+    }
+
+void findMax(node*)
+    {
+        if(node == NULL)
+            {
+                printf("Drzewo jest puste!\n");
+                return;
+            }
+        if(node->right != NULL)
+            return findMax(node->right);
+        else
+            printf("Ostatni rekord to %s\n", node->entry.stdName);
+        return;
+    }
+
+void moveInOrder(node*)
+    {
+        if(node != NULL)
+            {
+                moveInOrder(node->left);
+                printf("%s\n", node->entry.StdName);
+                moveInOrder(node->right);
+            }
+        return;
+    }
