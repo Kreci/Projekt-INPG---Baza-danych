149a6c6416975a2e0bb7218d5856e7716c1f63d8
Kreci <olson3344@gmail.com>
2016-05-06 19:41:32 +0200

    Added files via upload

--- data.h
+++ data.h
@@ -1,18 +1,25 @@
 #ifndef DATA_H
 #define DATA_H
-#define LENGHT 20
+#define LENGTH 20
+
 typedef struct note // data structure
 {
-    char stdName[LENGHT];
-    char latinName[LENGHT];
+    char stdName[LENGTH];
+    char latinName[LENGTH];
 }data;
+
 typedef struct junction //node structure
 {
     data entry;
     struct junction* left;
     struct junction* right;
 }node;
+
 int compare(data, data); // compares entries
-void display(data); // displays entry
-void fDisplay(node*);   //displays whole tree
+void display(data); 	 // displays entry
+void fDisplay(node*);    // displays whole tree
+void findMin(node*);	 // displays first (alphabetically) entry
+void findMax(node*); 	 // displays last (alpabetically) entry
+void moveInOrder(node*); // moves in order through the tree
+
 #endif
