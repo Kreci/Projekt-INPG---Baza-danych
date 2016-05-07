10c050fa23b07706b4cad661df7036c38b94526a
Kreci <olson3344@gmail.com>
2016-05-07 18:29:22 +0200

    Added files via upload
    Funkcja wstawiajaca nowy element w odpowiednie miejsce w drzewie

---
+++ insertnode.c
@@ -0,0 +1,40 @@
+void insertNode(node **root)                                        //Funkcja wstawia wezel w odpowiednie miejsce drzewa
+    {
+        node *new_node = createNode();                       //Potrzebna funkcja createNode() alokujaca pamiec nowego elementu
+        node *temp = NULL;                                   //i wypelniajaca go odpowiednimi danymi (obsluga pliku)
+
+        if(*root == NULL)
+            {
+                *root = new_node;
+                return;
+            }
+
+        temp = *root;
+        while(temp != NULL)
+            {
+                if(compare(new_node->entry, temp->entry) == -1)
+                    {
+                        if(temp->left != NULL)
+                            temp = temp->left;
+                        else
+                            break;
+                    }
+                else if(compare(new_node->entry, temp->entry) == 1)
+                    {
+                        if(temp->right != NULL)
+                            temp = temp->right;
+                        else
+                            break;
+                    }
+                else
+                    {
+                        printf("Ten rekord juz istnieje\n");
+                        return;
+                    }
+            }
+        if(compare(new_node->entry, temp->entry) == -1)
+            temp->left = new_node;
+        else if(compare(new_node->entry, temp->entry) == 1)
+            temp->right = new_node;
+        return;
+    }
