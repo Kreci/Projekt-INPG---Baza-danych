a9dfc19f23444b3da29803f52b875c8012fbec7a
Kreci <olson3344@gmail.com>
2016-05-08 23:12:43 +0200

    Update data.h

--- data.h
+++ data.h
@@ -23,5 +23,6 @@ void findMax(node*); 	 // displays last (alpabetically) entry
 int browse_tree(node *head)       //Umożliwia chodzenie po drzewie galez po galezi
 void findRecord(node*);     //wyszukuje element o zadanej nazwie gatunkowej
 void insertNode(node**);    //wstawia wezel w odpowiednie miejsce w drzewie
+int nodeNumber(node *);     //zwraca ilosc elementow w drzewie
 
 #endif
