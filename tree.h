c869bec0eda36ef8a40bf2b6d9de18603c9752b9
Pegas868 <kuba.pegza@gmail.com>
2016-05-11 08:39:11 +0200

    Create tree.h

---
+++ tree.h
@@ -0,0 +1,8 @@
+#ifndef BRZOZA
+#define BRZOZA
+void findMin(node*);	 // displays first (alphabetically) entry
+void findMax(node*); 	 // displays last (alpabetically) entry
+int browse_tree(node *head);       //Umożliwia chodzenie po drzewie galez po galezi
+void findRecord(node*);     //wyszukuje element o zadanej nazwie gatunkowej
+int nodeNumber(node*);      //Podaje liczbe wpisow
+#endif
