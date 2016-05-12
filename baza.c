fa12537bb93a40e865c97e96fb2a430821a2c5fe
szymex678 <kamuda.sz@gmail.com>
2016-05-12 15:28:13 +0200

    Update baza.c
    Dokonczona funkcja dodajaca informacje do drzewa z bazy danych w pliku. Do testów...
    Pojawia się problem: Nie wszystkie gatunki mają nazwe zwyczajową, która ( z tego co pamietam) jest kluczem według ktorego budujemy drzewo

--- baza.c
+++ baza.c
@@ -1,6 +1,11 @@
-// Do skonczenia
+// Do testow
+#include <stdio.h>
+#include <stdlib.h>
+#include "data.h"
+#include "tree.h"
+#include "menu.h"
 
-int zawiera(char *linia, char *klucz) // sprawdza czy linia zawiera slowo kluczowe np rodzina, rodzaj itp. 
+int zawiera(char *linia, char *klucz) // sprawdza czy linia zawiera slowo kluczowe np rodzina, rodzaj itp.
 {                                     //zwraca 0 jesli nie znaleziono lub indeks tablicy charow wksazjuacy na koniec wyszukanego klucza
   int i=0, j=0;
   while(linia[i]!='\0')
@@ -14,42 +19,55 @@ int zawiera(char *linia, char *klucz) // sprawdza czy linia zawiera slowo kluczo
   }
   return 0;
 }
-int kopiujdane(char *linia, char *kopiujdo, int indekslinii)
+
+
+int kopiujdane(char *linia, char *kopiujdo, int indekslinii) //kupiuje dane od konca linii, lub wystapienia znaki '-'. Zwraca 0 jesli skopiowano do konca lini lub indeks tablizy charow po pauzie
 {
   int i=0
   while(linia[indekslinii]!='\0')
   {
     kopiujdo[i]=linia[indekslinii];
-    indekslinii++; 
+    indekslinii++;
     i++;
     if(linia[indekslinii]!='-') return ++indekslinii;
   }
   return 0;
 }
 
-
 //otwieranie pliki i obsluga bazy (wtepne)
 
+void wczytajBaze(node ** root,  char* nazwapliku)
+{
+    FILE *plik;
+    if(plik=fopen(nazwapliku, "r")!=NULL)
+    {
+        printf("blad otwarcia plku");
+        return;
+    }
 
-char* nazwapliku
-FILE *plik;
-plik=fopen(nazwapliku, "r+")
-    
     char linia[100];
-    char actualrodzina[LENGTH];
-    char actualrodzaj[LENGTH];
     int indekslinii;
-    
     data dane;
+
     while(fscanf(plik, linia)!=EOF)
     {
-      if(indekslinii = zawiera(linia, "rodzina") kopiujdane(linia, actualrodzina, indekslinii);
-      if(indekslinii = zawiera(linia, "rodzaj")  kopiujdane(linia, actualrodzaj, indekslinii);
-      if(indekslinii = zawiera(linia, "\t\t\t\t")  
-      if(indekslinii = kopiujdane(linia, dane.latinname, indekslinii))
-      
-      
-    
+        if(indekslinii = zawiera(linia, "rodzina") kopiujdane(linia, dane.rodzina, indekslinii);
+        if(indekslinii = zawiera(linia, "rodzaj")  kopiujdane(linia, dane.rodzaj, indekslinii);
+        if(indekslinii = zawiera(linia, "\t\t\t\t")
+        {
+            if(indekslinii = kopiujdane(linia, dane.latinName, indekslinii)) //warunek sprawdzajacy czy istnieje nazwa zwyczajowa
+            {
+                kopiujdane(linia, data.stdName, indekslinii);
+            }
+            else kopiujdane("brak nazwy",data.stdName, 0); //jesli nie istnieje nazwa zwyczajowa, tutaj nie wiem jak bedzie z tworzeniem drzewa jesli nazwa zwyczajowa bedzie taka sama tzn "brak nazwy"
+
+            insertNode(root, &dane);
+        }
     }
-    
-    
+    fclose(plik);
+}
+
+
+
+
+
