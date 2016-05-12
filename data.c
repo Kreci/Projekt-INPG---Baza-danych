66597b7ba23beba10ce7a8768b98418d2bf62be2
Kreci <olson3344@gmail.com>
2016-05-12 14:47:00 +0200

    Update data.c

--- data.c
+++ data.c
@@ -6,32 +6,37 @@ int compare(data entry1, data entry2)       //Compares entries(surnames), return
 {
     int i;
     int code1, code2;
-    for(i=0; entry1.stdName[i]!='\0'||entry2.stdName[i]!='\0'; i++)
+    for(i=0; entry1.stdName[i] != '\0' || entry2.stdName[i] != '\0'; i++)
     {
         code1=entry1.stdName[i];
         code2=entry2.stdName[i];
-        if(code1>96)                        //In case of capital letters
-            code1-=32;
-        if(code2>96)
-            code2-=32;
-        if(code1>code2)
+        if(code1 > 96)                        //In case of capital letters
+            code1 -= 32;
+        if(code2 > 96)
+            code2 -= 32;
+        if(code1 > code2)
             return 1;
-        if(code2>code1)
+        if(code2 > code1)
             return -1;
     }
-    if(entry1.stdName[i]!='\0')
+    if(entry1.stdName[i] != '\0')
         return 1;
-    if(entry2.stdName[i]!='\0')
+    if(entry2.stdName[i] != '\0')
         return -1;
     return 0;
 }
 //*******************************************************************************************************************************
 void display(data entry)                //Displays name, surname and tel of entry, NOT node
 {
+    printf("Nazwa gatunkowa: ");
     puts(entry.stdName);
+    printf("Nazwa lacinska: ");
     puts(entry.latinName);
+    printf("Rodzina: ");
     puts(entry.rodzina);
+    printf("Rodzaj: ");
     puts(entry.rodzaj);
+    printf("Opis: ");
     puts(entry.opis);
     return;
 }
