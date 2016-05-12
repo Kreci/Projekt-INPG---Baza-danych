d5a85fc56ae390de7e78b0045488bf2e5d0dd9a7
Kreci <olson3344@gmail.com>
2016-05-12 14:48:08 +0200

    Create menu.c
    Daily Scrum:
    Dodałem menu, poprawiłem kilka funkcji, przetestowałem działanie podstawowych funkcji.

---
+++ menu.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+#include "data.h"
+
+void menu(node* root)
+    {
+        int choice;
+        do
+            {
+                #ifdef  WIN32
+                system("cls");
+                #else
+                system("clear");
+                #endif // WIN32
+
+                printf(" Witaj w bazie danych!\n"
+                        "**********************************************\n"
+                        " Wybierz funkcje\n"
+                        "----------------------------------------------\n"
+                        " [0] Koniec programu\n"
+                        " [1] Dodaj wezly\n"
+                        " [2] Wypisz zawartosc bazy\n"
+                        " [3] Wypisz pierwszy (alfabetycznie) rekord\n"
+                        " [4] Wypisz ostatni (alfabetycznie) rekord\n"
+                        "----------------------------------------------\n"
+                        " Twoj wybor: ");
+                scanf("%d", &choice);
+
+                #ifdef  WIN32
+                system("cls");
+                #else
+                system("clear");
+                #endif // WIN32
+
+                switch(choice)
+                    {
+                        case 1: addNodes(&root);
+                            break;
+                        case 2: fDisplay(root);
+                        case 3: findMax(root);
+                            break;
+                        case 4: findMin(root);
+                            break;
+                    }
+                if((choice >= 1) && (choice <= 7))
+                    {
+                        #ifdef WIN32
+                        system("pause");
+                        #else
+                        getchar();
+                        getchar();
+                        #endif
+                    }
+                }
+        while(choice);
+        printf("Do widzenia!\n");
+    }
