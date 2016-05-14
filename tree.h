f35f838f37a128bd296fb0892076cb3ebd18b83f
Kreci <olson3344@gmail.com>
2016-05-14 23:45:16 +0200

    Update tree.h

--- tree.h
+++ tree.h
@@ -10,5 +10,6 @@ void findMax(node*); 	              //displays last (alpabetically) entry
 int browse_tree(node *head);        //Umożliwia chodzenie po drzewie galez po galezi
 void findRecord(node*);             //wyszukuje element o zadanej nazwie gatunkowej
 int nodeNumber(node*);              //Podaje liczbe wpisow
+void deleteNode(node** root);       //Usuwa wskazany element
 
 #endif
