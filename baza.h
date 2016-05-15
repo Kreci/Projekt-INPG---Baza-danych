a656846dc0ab2deab78d99260f1776f6e094b110
szymex678 <kamuda.sz@gmail.com>
2016-05-16 00:12:18 +0200

    Update baza.h

--- baza.h
+++ baza.h
@@ -2,12 +2,14 @@
 #define OBSLUGA_PLIKU
 
 int zawiera(char *linia, char *klucz);                         // sprawdza czy linia zawiera slowo kluczowe np rodzina, rodzaj itp.
-                                                              //zwraca 0 jesli nie znaleziono lub indeks tablicy charow wksazjuacy na koniec wyszukanego klucza
+                                                               //zwraca 0 jesli nie znaleziono lub indeks tablicy charow wksazjuacy na koniec wyszukanego klucza
 
 int kopiujdane(char *linia, char *kopiujdo, int indekslinii);  //kupiuje dane od konca linii, lub wystapienia znaki '-'. 
-                                                              //Zwraca 0 jesli skopiowano do konca lini lub indeks tablizy charow po pauzie
+                                                               //Zwraca 0 jesli skopiowano do konca lini lub indeks tablizy charow po pauzie
 
 void wczytajBaze(node ** root,  char* nazwapliku);             //otwiera plik z baza oraz wczytuje z niego dane do drzewa
+
+void zapiszDane(data dane,  char* nazwapliku);                 //zapisuje do pliku elemrnt bazy          
 
 
 #endif // OBSLUGA_PLIKU
