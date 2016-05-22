7370c6969dbf3792f0203015bbe47fa35c806bda
Kreci <olson3344@gmail.com>
2016-05-22 21:53:57 +0200

    Update tree.h

--- tree.h
+++ tree.h
@@ -7,7 +7,6 @@ node *createNode(data* input);      //alokuje pamiec na gałaz drzewa i uzupelni
 void addNodes(node** root);         //kompletna funkcja tworzaca drzewo, (alokacja pamieci, wczytanie danych z klawiatury i dolaczenie ich do drzewa)
 void findMin(node*);	              //displays first (alphabetically) entry
 void findMax(node*); 	              //displays last (alpabetically) entry
-void findRecord(node*);             //wyszukuje element o zadanej nazwie gatunkowej
 int nodeNumber(node*);              //Podaje liczbe wpisow
 void mDestroy(node *);        //Usuwa wskazany element bez usuwania przypadkowych elementow
 void freeMem(node*);                //Zwalnia pamiec
