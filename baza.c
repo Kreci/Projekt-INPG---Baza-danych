1a45d2265ef888079aef57ed404d68c90c8f78cb
szymex678 <kamuda.sz@gmail.com>
2016-05-23 23:17:32 +0200

    Update baza.c

--- baza.c
+++ baza.c
@@ -92,9 +92,9 @@ void zapiszDane(data dane,  char* nazwapliku)
         return;
     }
 
-    fprintf(plik, "\t%%R Rodzina %s\n", dane.rodzina);
-    fprintf(plik, "\t\t%%G %s\n", dane.rodzaj);
-    fprintf(plik, "\t\t\t%%L %s %%S %s\n", dane.latinName, dane.stdName);
+    fprintf(plik, "\t%%R Rodzina %s%%\n", dane.rodzina);
+    fprintf(plik, "\t\t%%G %s%%\n", dane.rodzaj);
+    fprintf(plik, "\t\t\t%%L %s %%S %s%%\n", dane.latinName, dane.stdName);
     fclose(plik);
 }
 
